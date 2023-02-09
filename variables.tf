93845e23eaff76717d09e3e1064c663035328c02
root <root@ip-172-31-40-26.eu-west-1.compute.internal>
2023-02-09 05:26:21 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type =string
+  default = "eu-west-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-0f5c9acedd4f688d1"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-0588cb93982d9b129"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-0ca9dc97945b9f6c0"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "vpc-06423e604460b678e"
+   
+ }
